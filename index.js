9eaf76fe2b9733d8a6be9de9641991c5e512cdb3
Heather Booker <heather.booker@hotmail.com>
2017-01-17 12:06:44 -0500

    ask for name of file to be parsed

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const fs = require('fs');
+const readline = require('readline');
 
 
-fs.readFile('./Bookmarks.html', 'utf8', (err, data) => {
-
-  if (err) {
-    return console.error(err);
-  }
-
-  const bookmarks = parseBookmarks(data);
-
-  fs.writeFile('bookmarks.json', JSON.stringify(bookmarks, null, 2));
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
+rl.question('name the *.html file containing your bookmarks:\n', response => {
+
+  rl.close();
+
+  let fileName = response.trim();
+  if (!/\.html/.test(response)) {
+    fileName += '.html';
+  }
+
+  fs.readFile(fileName, 'utf8', (err, data) => {
+
+    if (err) {
+      return console.error(err);
+    }
+
+    const bookmarks = parseBookmarks(data);
+
+    fs.writeFile('bookmarks.json', JSON.stringify(bookmarks, null, 2));
+  });
+
+});
 
 function parseBookmarks(data) {
 
