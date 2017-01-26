20a8a21d13e934d98fab9506d0f415b14919feb6
ebb-tide <denizk@gmail.com>
2017-01-26 13:52:10 -0500

    transforms to an express app

---
+++ app.js
@@ -0,0 +1,77 @@
+var path = require('path');
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var csurf = require('csurf');
+var multer = require('multer');
+var upload = multer();
+var url= require('url');
+var fs= require('fs'); 
+
+var http = require('http');
+var https = require('https');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+var server = require('http').Server(app);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
+    flags: 'a'
+})
+app.use(morgan('combined', {
+    stream: accessLogStream
+}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/vis.html',function(req, res) {
+    res.status(200);
+    res.render('vis')
+  })
+  
+app.use('/', routes); // renders index to / request
+app.use('/users', users);
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+if (process.env.NODE_ENV !== 'test') {
+    app.use(csurf());
+    app.use(function(request, response, next) {
+        response.locals.csrftoken = request.csrfToken();
+        next();
+    });
+}
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = {
+    app: app,
+    server: server
+};
---
+++ public/js/vis.js
@@ -0,0 +1,116 @@
+
+var width = window.innerWidth;
+var height= window.innerHeight;
+var svg = d3.select("svg")
+.attr('height', height)
+.attr('width', width)
+.call(d3.zoom().on("zoom", function () {
+  svg.attr("transform", d3.event.transform)
+}))
+.append('g');
+
+// var color = d3.scaleOrdinal(d3.schemeCategory20);
+color=['#ee6e73','#78909C','#ee6e73','#78909C'];
+
+var simulation = d3.forceSimulation()
+.force("link", d3.forceLink().id(function(d) { return d.id; })
+.distance(function(d){return d.value== 1? 300: 300;}))
+.force("charge", d3.forceManyBody())
+.force("center", d3.forceCenter(width / 2, height / 2));
+
+// function makerandomlinks(innode, tags) {
+//   outlinks= []
+// for (i=0; i<innode.length; i++) {
+//       linkto= Math.floor(Math.random()*tags.length);
+//       outlinks.push( {'source':innode[i].id, 'target': tags[linkto].id, 'value': 1} )
+//   }
+//   return outlinks
+// }
+// d3.json("links.json", function(error, nodette) {
+//   tags=[]
+//   for (i=0; i<nodette.length/3; i++){
+//     tags.push({'title':"TAG"+i, 'id':"TAG"+i, 'group':2 })
+//   }
+
+d3.json("/rcrs/awesomelinks.json", function(error, graph) {
+  graph.nodes.push({"id": "AWESOME- Dot Files","title": "AWESOME- Dot Files","group": 2})
+  var boo= graph.nodes.filter(function(elt){return elt.group==2})
+  for (var i=0; i<boo.length; i++){
+    graph.links.push({'source':"AWESOME- Dot Files", 'target':boo[i] , 'value': 2})
+  }
+  if (error) throw error;
+
+  var link = svg.append("g")
+  .attr("class", "links")
+  .selectAll("line")
+  .data(graph.links)
+  .enter().append("line")
+  .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+  var node = svg.append("g")
+  .selectAll("circle")
+  .data(graph.nodes)
+  .enter()
+  .append('g')
+  .attr("class", function(d) { return d.group==1 ? "nodes": "tag"; })
+
+  var circle=node.append("circle")
+  .attr("r", 7)
+  .attr("fill", function(d) { return color[d.group];})
+
+  var txt=node.append("text")
+  .text(function(d) { return d.title; })
+  .attr('class','txt');
+
+  var url=node.append("text")
+  .text(function(d) { return d.group==1 ? d.id:""; })
+  .attr('class','url');
+
+  circle.call(d3.drag()
+  .on("start", dragstarted)
+  .on("drag", dragged)
+  .on("end", dragended));
+
+  simulation.nodes(graph.nodes)
+  .on("tick", ticked)
+  .force("link")
+  .links(graph.links);
+
+  function ticked() {
+    link
+    .attr("x1", function(d) { return d.source.x; })
+    .attr("y1", function(d) { return d.source.y; })
+    .attr("x2", function(d) { return d.target.x; })
+    .attr("y2", function(d) { return d.target.y; });
+    txt
+    .attr("x", function(d) { return d.x+10; })
+    .attr("y", function(d) { return d.y+10; });
+    url
+    .attr("x", function(d) { return d.x+10; })
+    .attr("y", function(d) { return d.y+25; });
+    circle
+    .attr("cx", function(d) { return d.x; })
+    .attr("cy", function(d) { return d.y; });
+  }
+});
+
+function dragstarted(d) {
+  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+  d.fx = d.x;
+  d.fy = d.y;
+}
+
+function dragged(d) {
+  d.fx = d3.event.x;
+  d.fy = d3.event.y;
+}
+
+function dragended(d) {
+  if (!d3.event.active) simulation.alphaTarget(0);
+  d.fx = d.x;
+  d.fy = d.y;
+}
+
+function resetted() {
+// this should probably do something at some point
+}
