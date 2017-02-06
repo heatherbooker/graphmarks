7dc071899515b9c61b43c5804d683f1b7f70e810
Heather Booker <heather.booker@hotmail.com>
2017-02-05 19:48:56 -0500

    Parse bookmarks from chrome api into d3 format

--- chrome-extension/graphmarks.js
+++ chrome-extension/graphmarks.js
@@ -1,3 +1,29 @@
 chrome.bookmarks.getTree(function(bookmarks) {
-  console.log(bookmarks);
+
+  const nodes = [{id: 'root', title: 'root', children: []}];
+  const bkmrksBar = bookmarks[0].children[0];
+  const bkmrks = bookmarks[0].children[1];
+
+  addNodes(bkmrks, nodes[0].title, nodes[0].children, nodes);
+  console.log(nodes);
 });
+
+function addNodes(bkmrk, parent, parentsChildren, nodes) {
+
+  const newNode = {
+    id: bkmrk.url || bkmrk.title,
+    title: bkmrk.title,
+    parent,
+    children: [],
+    layer: 1
+  };
+  nodes.push(newNode);
+  parentsChildren.push(newNode.id);
+
+  if (typeof bkmrk.children !== 'undefined') {
+    bkmrk.children.forEach(child => {
+      addNodes(child, bkmrk.title, newNode.children, nodes);
+    });
+  }
+}
+
