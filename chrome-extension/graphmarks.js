aa8e16b6d1d2ad071e05d4eb05ef5d52d779a4fb
Heather Booker <heather.booker@hotmail.com>
2017-02-05 20:50:33 -0500

    Add layer for all and parent for root nodes

--- chrome-extension/graphmarks.js
+++ chrome-extension/graphmarks.js
@@ -1,28 +1,28 @@
 chrome.bookmarks.getTree(function(bookmarks) {
 
-  const nodes = [{id: 'root', title: 'root', children: []}];
+  const nodes = [{id: 'root', title: 'root', parent: '', children: [], layer: 0}];
   const bkmrksBar = bookmarks[0].children[0];
   const bkmrks = bookmarks[0].children[1];
 
-  addNodes(bkmrks, nodes[0].title, nodes[0].children, nodes);
+  addNodes(bkmrks, nodes[0].title, nodes[0].children, nodes, 1);
   console.log(nodes);
 });
 
-function addNodes(bkmrk, parent, parentsChildren, nodes) {
+function addNodes(bkmrk, parent, parentsChildren, nodes, layer) {
 
   const newNode = {
     id: bkmrk.url || bkmrk.title,
     title: bkmrk.title,
     parent,
     children: [],
-    layer: 1
+    layer
   };
   nodes.push(newNode);
   parentsChildren.push(newNode.id);
 
   if (typeof bkmrk.children !== 'undefined') {
     bkmrk.children.forEach(child => {
-      addNodes(child, bkmrk.title, newNode.children, nodes);
+      addNodes(child, bkmrk.title, newNode.children, nodes, layer+1);
     });
   }
 }
